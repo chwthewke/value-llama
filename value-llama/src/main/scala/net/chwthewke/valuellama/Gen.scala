d95b281ce33a8c23a0477f2f6bc3eb8986d99679
Chewie <chwthewke@gmail.com>
2016-03-14 08:00:11 +0100

    [value-llama] Remove MonadState, replace Result/MonadPlus with Disjunction

--- value-llama/src/main/scala/net/chwthewke/valuellama/Gen.scala
+++ value-llama/src/main/scala/net/chwthewke/valuellama/Gen.scala
@@ -5,55 +5,29 @@ import scala.collection.generic.CanBuildFrom
 import scala.annotation.tailrec
 import scala.language.higherKinds
 import scalaz.{ Lens => _, _ }, Scalaz._
-import scalaz.syntax.isEmpty.ToIsEmptyOps
 
-import MonadStateSyntax._
+class Gen[S, +A]( val runGen : ( GenParams, S ) => ( S, GenFailure \/ A ) ) extends AnyVal
 
-class Gen[S, +A]( val runGen : ( GenParams, S ) => ( S, Result[A] ) ) extends AnyVal
+trait GenInstances {
 
-trait GenFunctions0 {
-
-  def constResult[S, A]( ra : => Result[A] ) : Gen[S, A] =
-    Gen( ( _, s ) => ( s, ra ) )
-
-}
-
-trait GenInstances extends GenFunctions0 {
-
-  implicit def GenInstance[S] : MonadState[Gen[S, ?], S] with MonadPlus[Gen[S, ?]] with BindRec[Gen[S, ?]] =
-    new MonadState[Gen[S, ?], S] with MonadPlus[Gen[S, ?]] with BindRec[Gen[S, ?]] {
-      override def init : Gen[S, S] = Gen( ( p, s ) => ( s, s.point[Result] ) )
-
-      override def get : Gen[S, S] = init
-
-      override def put( s : S ) : Gen[S, Unit] = Gen( ( p, _ ) => ( s, ().point[Result] ) )
-
-      override def modify( f : S => S ) : Gen[S, Unit] = Gen( ( p, s ) => ( f( s ), ().point[Result] ) )
-
-      // TODO stack-safety
+  implicit def GenInstance[S] : Monad[Gen[S, ?]] with BindRec[Gen[S, ?]] =
+    new Monad[Gen[S, ?]] with BindRec[Gen[S, ?]] {
+      // TODO stack-safety?
       override def bind[A, B]( fa : Gen[S, A] )( f : A => Gen[S, B] ) : Gen[S, B] =
         Gen( ( p, s ) => {
           val ( s1, r1 ) = fa.runGen( p, s )
-          r1.value.cata( a => f( a ).runGen( p, s1 ), ( s1, mempty[Result, B] ) )
+          r1.fold( e => ( s1, -\/( e ) ), a => f( a ).runGen( p, s1 ) )
         } )
 
-      override def empty[A] : Gen[S, A] = constResult( mempty[Result, A] )
-
-      override def plus[A]( a : Gen[S, A], b : => Gen[S, A] ) : Gen[S, A] =
-        Gen( ( p, s ) => {
-          val ( s1, r1 ) = a.runGen( p, s )
-          if ( r1.isEmpty ) b.runGen( p, s1 ) else ( s1, r1 )
-        } )
-
-      override def point[A]( a : => A ) : Gen[S, A] = constResult( a.point[Result] )
+      override def point[A]( a : => A ) : Gen[S, A] = Gen( ( p, s ) => ( s, \/-( a ) ) )
 
       override def tailrecM[A, B]( f : A => Gen[S, A \/ B] )( a : A ) : Gen[S, B] = Gen { ( p, s ) =>
         @tailrec
-        def go( s : S, a : A ) : ( S, Result[B] ) =
+        def go( s : S, a : A ) : ( S, GenFailure \/ B ) =
           f( a ).runGen( p, s ) match {
-            case ( s, Result( None ) )             => ( s, Result( None ) )
-            case ( s, Result( Some( \/-( b ) ) ) ) => ( s, Result( Some( b ) ) )
-            case ( s, Result( Some( -\/( a ) ) ) ) => go( s, a )
+            case ( s1, -\/( err ) )       => ( s1, -\/( err ) )
+            case ( s1, \/-( \/-( b ) ) )  => ( s1, \/-( b ) )
+            case ( s1, \/-( -\/( a1 ) ) ) => go( s1, a1 )
           }
 
         go( s, a )
@@ -62,42 +36,34 @@ trait GenInstances extends GenFunctions0 {
 
 }
 
-trait StateFunctions extends GenInstances {
-
-  @inline
-  private def M[S] : MonadState[Gen[S, ?], S] = MonadState[Gen[S, ?], S]
-
-  def get[S] : Gen[S, S] = M.get
-
-  def gets[S, A]( f : S => A ) : Gen[S, A] = M[S].map( M[S].get )( f )
-
-  def put[S]( s : S ) : Gen[S, Unit] = M.put( s )
-
-  def modify[S]( f : S => S ) : Gen[S, Unit] = M.modify( f )
-}
-
-trait GenFunctions extends StateFunctions {
+trait GenFunctions {
 
   def long : Gen[GenState, Long] = zoom( GenState.seed )( stateGen( _.long ) )
+
   def long( a : Long, b : Long ) : Gen[GenState, Long] = zoom( GenState.seed )( stateGen( _.long( a, b ) ) )
 
   def int : Gen[GenState, Int] = long.map( _.toInt )
+
   def int( a : Int, b : Int ) : Gen[GenState, Int] = long( a, b ).map( _.toInt )
 
   def double : Gen[GenState, Double] = zoom( GenState.seed )( stateGen( _.double ) )
 
   private def withParams[S, A]( g : GenParams => Gen[S, A] ) : Gen[S, A] =
-    Monad[Gen[S, ?]].bind( Gen( ( p, s ) => ( s, p.pure[Result] ) ) )( g )
+    Gen[S, GenParams]( ( p, s ) => ( s, \/-( p ) ) ).flatMap( g )
 
   def sized[S, A]( g : Int => Gen[S, A] ) : Gen[S, A] =
     withParams( g compose ( _.size ) )
 
-  // TODO TC-driven function -> Gen
-  // (p =>)? (s =>)? (s?, a/Result[a]) ~> Gen
+  private def modifyParams[S, A]( f : GenParams => GenParams, g : Gen[S, A] ) : Gen[S, A] =
+    Gen( ( p, s ) => g.runGen( f( p ), s ) )
 
-  def stateGen[S, A]( st : S => ( S, A ) ) : Gen[S, A] = stateM[Gen[S, ?], S, A]( st )
+  def resized[S, A]( s : Int, g : Gen[S, A] ) : Gen[S, A] = modifyParams( GenParams.size.set( s ), g )
 
-  def withState[S, A]( g : S => Gen[S, A] ) : Gen[S, A] = get[S].flatMap( g )
+  def retried[S, A]( n : Int, g : Gen[S, A] ) : Gen[S, A] = modifyParams( GenParams.maxFailures.set( n ), g )
+
+  def stateGen[S, A]( st : S => ( S, A ) ) : Gen[S, A] = Gen( ( p, s ) => st( s ).map( \/-( _ ) ) )
+
+  def withState[S, A]( g : S => Gen[S, A] ) : Gen[S, A] = Gen[S, S]( ( p, s ) => ( s, \/-( s ) ) ).flatMap( g )
 
   def xmapState[S, T, A]( g : Gen[S, A] )( b : T => S, f : S => T ) : Gen[T, A] =
     Gen { ( p, t ) =>
@@ -143,10 +109,11 @@ trait GenFunctions extends StateFunctions {
 }
 
 object Gen extends GenInstances with GenFunctions {
+
   implicit class GenOps[A]( val self : Gen[GenState, A] ) {
-    def eval( p : GenParams ) : Result[A] =
+    def eval( implicit p : GenParams ) : GenFailure \/ A =
       self.runGen( p, GenState( Seed.random, 0 ) )._2
   }
 
-  def apply[S, A]( r : ( GenParams, S ) => ( S, Result[A] ) ) = new Gen( r )
+  def apply[S, A]( r : ( GenParams, S ) => ( S, GenFailure \/ A ) ) = new Gen( r )
 }
--- value-llama/src/main/scala/net/chwthewke/valuellama/types.scala
+++ value-llama/src/main/scala/net/chwthewke/valuellama/types.scala
@@ -9,8 +9,11 @@ import scalaz.{ Lens => _, _ }, Scalaz._
 case class GenState( seed : Seed, failures : Int )
 
 object GenState {
-  val failures : Lens[GenState, Int] = GenLens[GenState]( _.failures )
   val seed : Lens[GenState, Seed] = GenLens[GenState]( _.seed )
+  val failures : Lens[GenState, Int] = GenLens[GenState]( _.failures )
+
+  val incFail = failures.modify( _ + 1 )
+  val failOnce = failures.set( 1 )
 }
 
 case class GenParams( size : Int, maxFailures : Int )
@@ -20,37 +23,8 @@ object GenParams {
   val maxFailures : Lens[GenParams, Int] = GenLens[GenParams]( _.maxFailures )
 }
 
-class Result[+A]( val value : Option[A] ) extends AnyVal {
-  override def toString = s"Result($value)"
-}
+sealed trait GenFailure
+case object UnspecifiedFailure extends GenFailure
+case object FilterFailed extends GenFailure
+case class RetryFailed( f : GenFailure ) extends GenFailure
 
-object Result {
-  def apply[A]( a : Option[A] ) : Result[A] = new Result( a )
-
-  def unapply[A]( r : Result[A] ) : Option[Option[A]] = Some( r.value )
-
-  def option[A] : Iso[Result[A], Option[A]] = Iso.apply[Result[A], Option[A]]( _.value )( new Result( _ ) )
-
-  def value[A] : Prism[Result[A], A] = option.composePrism( monocle.std.option.some )
-
-  implicit val ResultInstance : MonadPlus[Result] with Traverse[Result] with BindRec[Result] with IsEmpty[Result] =
-    new MonadPlus[Result] with Traverse[Result] with BindRec[Result] with IsEmpty[Result] {
-      override def traverseImpl[G[_], A, B]( fa : Result[A] )( f : A => G[B] )( implicit G : Applicative[G] ) : G[Result[B]] =
-        fa.value.traverse( f ).map( new Result( _ ) )
-
-      override def tailrecM[A, B]( f : A => Result[A \/ B] )( a : A ) : Result[B] =
-        new Result( BindRec[Option].tailrecM( ( x : A ) => f( x ).value )( a ) )
-
-      override def point[A]( a : => A ) : Result[A] = new Result( a.point[Option] )
-
-      override def isEmpty[A]( fa : Result[A] ) : Boolean = fa.value.isEmpty
-
-      override def empty[A] : Result[A] = new Result( None )
-
-      override def plus[A]( a : Result[A], b : => Result[A] ) : Result[A] =
-        new Result( a.value <+> b.value )
-
-      override def bind[A, B]( fa : Result[A] )( f : A => Result[B] ) : Result[B] =
-        new Result( fa.value.flatMap( ( x : A ) => f( x ).value ) )
-    }
-}
