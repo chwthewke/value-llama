c8d8ba9cb00ca4ca96b69389f64d7bb8f48d7e04
Chewie <chwthewke@gmail.com>
2016-03-14 07:58:12 +0100

    [value-llama] Made Seed more consistent

--- value-llama/src/main/scala/net/chwthewke/valuellama/Seed.scala
+++ value-llama/src/main/scala/net/chwthewke/valuellama/Seed.scala
@@ -8,32 +8,11 @@ import scalaz._, Scalaz._
  *
  * http://burtleburtle.net/bob/rand/smallprng.html
  */
-sealed abstract class Seed {
-  protected val a : Long
-  protected val b : Long
-  protected val c : Long
-  protected val d : Long
-
-  /** Generate the next seed in the RNG's sequence. */
-  def next : Seed = {
-    import java.lang.Long.rotateLeft
-    val e = a - rotateLeft( b, 7 )
-    val a1 = b ^ rotateLeft( c, 13 )
-    val b1 = c + rotateLeft( d, 37 )
-    val c1 = d + e
-    val d1 = e + a
-    Seed( a1, b1, c1, d1 )
-  }
-
-  /** Reseed the RNG using the given Long value. */
-  def reseed( n : Long ) : Seed = {
-    val n0 = ( n >>> 32 ) & 0xffffffff
-    val n1 = n & 0xffffffff
-    var i = 0
-    var seed : Seed = Seed( a ^ n0, b ^ n1, c, d )
-    while ( i < 16 ) { seed = seed.next; i += 1 }
-    seed
-  }
+sealed class Seed private (
+    private val a : Long,
+    private val b : Long,
+    private val c : Long,
+    private val d : Long ) {
 
   /**
    * Generates a Long value.
@@ -46,7 +25,8 @@ sealed abstract class Seed {
    * Generates a long value in [origin; bound[
    *
    * @param origin the (inclusive) lower bound
-   * @param bound the (exclusive) upper bound
+   * @param bound  the (exclusive) upper bound
+   * @note If origin >= bound, this method behaves as long
    */
   def long( origin : Long, bound : Long ) : ( Seed, Long ) = {
     if ( origin < bound ) {
@@ -85,21 +65,38 @@ sealed abstract class Seed {
    * in the interval [0.0, 1.0).
    */
   def double : ( Seed, Double ) = ( next, ( d >>> 11 ) * 1.1102230246251565e-16 )
+
+  /** Generate the next seed in the RNG's sequence. */
+  private def next : Seed = {
+    import java.lang.Long.rotateLeft
+    val e = a - rotateLeft( b, 7 )
+    val a1 = b ^ rotateLeft( c, 13 )
+    val b1 = c + rotateLeft( d, 37 )
+    val c1 = d + e
+    val d1 = e + a
+    Seed( a1, b1, c1, d1 )
+  }
+
+  @tailrec
+  private def nextN( n : Int ) : Seed = {
+    if ( n <= 0 ) this
+    else next.nextN( n - 1 )
+  }
+
+  /** Reseed the RNG using the given Long value. */
+  def reseed( n : Long ) : Seed =
+    Seed( a ^ ( ( n >>> 32 ) & 0xffffffff ), b ^ ( n & 0xffffffff ), c, d ).nextN( 16 )
+
 }
 
 object Seed {
 
-  private[valuellama] case class apply( a : Long, b : Long, c : Long, d : Long ) extends Seed
+  def apply( a : Long, b : Long, c : Long, d : Long ) : Seed = new Seed( a, b, c, d )
 
   /** Generate a deterministic seed. */
-  def apply( s : Long ) : Seed = {
-    var i = 0
-    var seed : Seed = Seed( 0xf1ea5eed, s, s, s )
-    while ( i < 20 ) { seed = seed.next; i += 1 }
-    seed
-  }
+  def apply( s : Long ) : Seed = Seed( 0xf1ea5eed, s, s, s ).nextN( 20 )
 
   /** Generate a random seed. */
-  def random() : Seed = apply( scala.util.Random.nextLong )
+  def random : Seed = Seed( scala.util.Random.nextLong )
 
 }
